c06fdd57a9df72218c31f1676ea15606d001b757
TunNyein <tunnyein.ict@gmail.com>
2025-02-15 07:59:59 +0000

    first commit

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  cloud {
+    organization = "hellocloud-tf-global"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+    hostname = "app.terraform.io"
+
+    workspaces { 
+      name = "vcs-driven-workflow"
+    } 
+  }
+}
