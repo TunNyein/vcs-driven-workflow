7a0a1882f145285ae09fc435f467cc96ac97e331
TunNyein <93922636+TunNyein@users.noreply.github.com>
2025-02-15 22:57:02 +0630

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"  # CIDR block for your VPC
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "MyVPC"
-  }
-}
+#resource "aws_vpc" "main" {
+ # cidr_block = "10.0.0.0/16"  # CIDR block for your VPC
+ # enable_dns_support = true
+ # enable_dns_hostnames = true
+  #tags = {
+  #  Name = "MyVPC"
+  #}
+#}
